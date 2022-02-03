0620198a4cd5b750aa6f2f02381387fb3511216d
mrbroschat <mrbroschat@gmail.com>
2022-02-03 08:46:26 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,23 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Montlake Web</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title="Just getting started" />
+	<div>
+		<ul>
+			<li><a href=5Minute/5Minute.html>5-Minute Tours</a></li>
+			<li><a href=MontlakeBlog/default.html>Blog</a></li>
+			<li><a href=journals/journals.html>Travel Journals</a></li>
+			<li><a href=WebTopics/default.html>Web topics</a></li>
+			<li><a href=LongviewChristmas/LongviewChristmas.html><b><span style="{color:red}">Latest:</span></b>&nbsp;Christmas in Longview, WA</a></li>
+		</ul>
+	</div>
+
+    <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
